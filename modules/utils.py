ca15ae64e2dde24c76ccac339862aa378f7e2371
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2021-04-21 08:36:19 +0300

    fix type hints

---
+++ modules/utils.py
@@ -0,0 +1,15 @@
+import typing as t
+
+import pandas as pd
+
+
+def reformat_evaluation_results_to_single_dataframe(eval_results_list: t.List[t.Dict]) -> pd.DataFrame:
+    df = pd.DataFrame() 
+    for eval_result_meta in eval_results_list:
+        eval_results = [
+            (r.a1, r.a2, r.test_error.rmse, r.test_error.mae, eval_result_meta["model_name"])
+            for r in eval_result_meta["results"] 
+        ]
+        eval_results_df = pd.DataFrame(eval_results, columns=["a1", "a2", "rmse", "mae", "model"])
+        df = pd.concat([df, eval_results_df])
+    return df 
